bf2f8338ca48e463795e0da4ad547d4be03c0bc3
Reni Somoye <rensomoye@gmail.com>
2023-01-04 19:49:28 -0800

    commit tree f32f5577f9f08e9bb7d5b3545a576d83fe1a7e80

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback()
+}
+function returnsANamedFunction () {
+    return function namedFunction() {
+        console.log ("This is a named function")
+    }
+}
+function returnsAnAnonymousFunction () {
+    return function() {
+        console.log ("This is an anonymous function")
+    }
+}
